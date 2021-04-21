bc14bd84db8fb1e4f6a2c0e136817065e53f2ae3
thiagosimi10 <53903396+thiagosimi10@users.noreply.github.com>
2021-04-21 11:56:09 -0300

    add Vue in project

---
+++ frontend/vue-app/src/views/Main.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="am-wrapper">
+        <side-bar></side-bar>
+    </div>
+</template>
+import Sidebar from '@/components/Sidebar/Sidebar.vue';
+export default {
+    components: {
+        Sidebar
+    },
+    data() {
+        return {
+            showModal: false,
+        };
+    },
+};
+</script>
